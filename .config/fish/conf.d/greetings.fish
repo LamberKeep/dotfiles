0883376cf716418a7e35b919877a51db591a9a14
LamberKeep <LamberKeep@gmail.com>
2024-05-18 20:14:45 +0300

    Set fish greetings time to 24 hours format

--- .config/fish/conf.d/greetings.fish
+++ .config/fish/conf.d/greetings.fish
@@ -4,7 +4,7 @@ function fish_greeting
 	#echo
 	#echo -e "$(shuf -n 1 $HOME/.config/fish/conf.d/greetings.txt)"
 	#echo
-       	echo -e "Date\t$(date)"
+       	echo -e "Date\t$(date -R)"
 	echo -e "Uptime\t$(uptime -p | cut -b 4-)"
 	#echo -e "Public IP\t$(curl http://icanhazip.com/ -s)"
 	echo
